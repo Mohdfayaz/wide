c4f848cf6e78e0b9cbdc9eb691f786bf2dc20f83
Mohammed Fayaz z <mohammedfayazfn173148@gmail.com>
2025-08-07 16:11:42 +0530

    final upadtes

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function showAlert() {
   alert("Demo request submitted!");
 }
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("show");
+}
+function playVideo() {
+  alert("Playing video review… (embed your video modal or link here)");
+}
